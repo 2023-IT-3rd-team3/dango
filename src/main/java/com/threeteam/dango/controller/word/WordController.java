99ba63e256454f4c2cc2fb00e3537044dcd7672c
seop0914 <seop0914@gmail.com>
2023-12-26 09:27:12 +0900

    [Update] VO

--- src/main/java/com/threeteam/dango/controller/word/WordController.java
+++ src/main/java/com/threeteam/dango/controller/word/WordController.java
@@ -1,5 +1,8 @@
 package com.threeteam.dango.controller.word;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.threeteam.dango.domain.user.UserVO;
 import com.threeteam.dango.mapper.word.WordMapper;
 import com.threeteam.dango.service.word.WordService;
 import com.threeteam.dango.service.word.WrongService;
@@ -20,19 +24,38 @@ public class WordController {
 	@Autowired
 	WrongService wrongService;
 	
-	@GetMapping("/new/{wordlevel}")
-	public String list(@PathVariable("wordlevel") Integer wordlevel, Model model) {
-		model.addAttribute("sentenceList", wordService.getSentenceList(wordlevel, "aaa"));
-		model.addAttribute("userId", "aaa");
+	private UserVO getSessionUser(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		UserVO userInfo = (UserVO)session.getAttribute("userinfo");
+		
+		return userInfo;
+	}
+	
+	@GetMapping("/new/{wordLevel}")
+	public String list(@PathVariable("wordLevel") Integer wordLevel, Model model, HttpServletRequest request) {
+		UserVO userInfo = getSessionUser(request);
+		if(userInfo == null)
+			return "/user/login";
+		
+		model.addAttribute("sentenceList", wordService.getSentenceList(wordLevel, userInfo.getUserid()));
+		model.addAttribute("userId", userInfo.getUserid());
 		return "word/newWord";
 	}
 	@GetMapping("/new")
-	public String selectLevel() {
+	public String selectLevel(HttpServletRequest request) {
+		UserVO userInfo = getSessionUser(request);
+		if(userInfo == null)
+			return "/user/login";
+		
 		return "word/newWordLevelSelect";
 	}
 	@GetMapping("/wrong")
-	public String wrongNote(Model model) {
-		model.addAttribute("sentenceList", wrongService.getWrongAllByUserId("aaa"));
+	public String wrongNote(Model model, HttpServletRequest request) {
+		UserVO userInfo = getSessionUser(request);
+		if(userInfo == null)
+			return "/user/login";
+		
+		model.addAttribute("sentenceList", wrongService.getWrongAllByUserId(userInfo.getUserid()));
 		return "word/wrongNote";
 	}
 }
